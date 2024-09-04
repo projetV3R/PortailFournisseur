fc321a2a2ca7ceea560e506016af108d4abc6d33
Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
2024-09-04 09:33:49 -0400

    Initialisation iconify

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>@yield('title')</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  
+  <script src="https://code.iconify.design/3/3.0.0/iconify.min.js"></script>
+
   @vite('resources/css/app.css') 
   <link rel="stylesheet" href="{{ asset('style.css') }}">
   <link rel="shortcut icon" type="image/png" href="{{ asset('img/apple-icon-72x72.png') }}"/>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,5 +5,5 @@
 @section('contenu')
     <h1 class="bg-red-500 text-xl">Test</h1>
     
-   
+    <span class="iconify" data-icon="mdi:home" data-inline="false"></span>
 @endsection
