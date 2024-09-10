338361398fc3675659a505d0d5d700b5860aeb56
Mousse <143189920+IamMousse@users.noreply.github.com>
2024-09-10 18:39:45 -0400

    wip layout footer
    -tentative de faire version mobile bug
    -redressage texte droit bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,25 +24,25 @@
   <footer class="bg-gray-200">
     <div class="flex flex-row">
       <div class="flex flex-col justify-self-start items-center w-2/5 mt-8 m-5">
-        <div class="flex justify-self-around">
+        <div class="flex justify-left">
           <a href="https://www.v3r.net/" class="mx-8">
-            <img src="https://www.v3r.net/wp-content/themes/v3r/Images/icons/logo-v3r-v2017.svg" alt="V3R Logo" class="w-20 h-20"/>
+            <img src="https://www.v3r.net/wp-content/themes/v3r/Images/icons/logo-v3r-v2017.svg" alt="V3R Logo" class="w-20 h-20" />
           </a>
-          <ul>
+          <ul class="text-base font-raleway text-gray-500">
             <li>
-              <div class="text-base font-raleway-bold text-gray-500">
+              <div class="font-raleway-bold">
                 <h4>Ville de Trois-Rivières</h4>
               </div>
             </li>
             <li>
-              <div class="text-base font-raleway text-gray-400">
+              <div class="">
                 <a class="mx-2" href="https://www.google.ca/maps/place/H%C3%B4tel+de+ville+de+Trois-Rivi%C3%A8res/@46.3430042,-72.545511,17z/data=!4m14!1m7!3m6!1s0x41aa0c6a9ae1712b:0xc5f7bf52c7282858!2sH%C3%B4tel+de+ville+de+Trois-Rivi%C3%A8res!8m2!3d46.3435631!4d-72.5429808!16s%2Fg%2F11bwpv19ny!3m5!1s0x41aa0c6a9ae1712b:0xc5f7bf52c7282858!8m2!3d46.3435631!4d-72.5429808!16s%2Fg%2F11bwpv19ny?entry=ttu&g_ep=EgoyMDI0MDkwNC4wIKXMDSoASAFQAw%3D%3D" class="hover:underline me-4 md:me-6">
                   1325, place de l'Hôtel-de-Ville, C.P.368 Trois-Rivières, QC G9A 5H3
                 </a>
               </div>
             </li>
             <li>
-              <div class="text-lg font-raleway text-gray-600">
+              <div class="text-lg text-gray-600">
                 <h4>
                   <b>
                     Téléphone:
@@ -54,60 +54,64 @@
               </div>
             </li>
             <li>
-              <div class="text-base font-raleway text-gray-400">
+              <div class="">
                 <a class="mx-2" href="tel:18333742002" class="hover:underline me-4 md:me-6">Canada ou États-Unis: 1833 374-2002</a>
               </div>
             </li>
             <li>
-              <div class="text-base font-raleway text-gray-400">
+              <div class="">
                 <a class="mx-2" href="mailto:311@v3r.net" class="hover:underline me-4 md:me-6">Courriel: 311@v3r.net</a>
               </div>
             </li>
           </ul>
         </div>
       </div>
-      <div class="w-1/5 items-center mt-8">
-        Ajouter les 3 section avec >
+      <div class="w-1/5 p-10 text-gray-500 text-base bg-red-500 hidden">
+        <div class="flex flex-row justify-left">
+          <span class="iconify size-5" data-icon="weui:arrow-filled" data-inline="false"></span>
+          <a class="mx-2" href="https://www.v3r.net/a-propos-de-la-ville/communications" class="hover:underline">Communications</a>
+        </div>
+        <div class="flex flex-row justify-left">
+          <span class="iconify size-5" data-icon="weui:arrow-filled" data-inline="false"></span>
+          <a class="mx-2" href="https://www.v3r.net/faq" class="hover:underline">FAQ</a>
+        </div>
+        <div class="flex flex-row justify-left">
+          <span class="iconify size-5" data-icon="weui:arrow-filled" data-inline="false"></span>
+          <a class="mx-2" href="https://www.tourismetroisrivieres.com/fr" class="hover:underline">Tourisme</a>
+        </div>
       </div>
-      <div class="flex flex-col justify-end w-2/5 m-5 mb-10">
-          <ul class="text-base font-raleway">
-            <li>
-              <div class="flex flex-row flex justify-end text-small font-raleway text-gray-600 mb-4">
-                <a class="mx-2" href="https://www.v3r.net/a-propos-de-la-ville/communications/infolettre">
-                  <span class="iconify size-10" data-icon="mdi:email-newsletter" data-inline="false"></span>
-                </a>
-                <a class="mx-2" href="https://www.facebook.com/villetroisrivieres" class="">
-                  <span class="iconify size-10" data-icon="akar-icons:facebook-fill" data-inline="false"></span>
-                </a>
-                <a class="mx-2" href="https://www.instagram.com/villedetroisrivieres/" class="">
-                  <span class="iconify size-10" data-icon="bi:instagram" data-inline="false"></span>
-                </a>
-                <a class="mx-2" href="https://www.linkedin.com/company/ville-de-trois-rivi-res" class="">
-                  <span class="iconify size-10" data-icon="ri:linkedin-fill" data-inline="false"></span>
-                </a>
-                <a class="mx-2" href="https://www.youtube.com/channel/UC4UyW0CoFiJaFCFaOzoQQ5w" class="">
-                  <span class="iconify size-10" data-icon="fa:youtube" data-inline="false"></span>
-                </a>
-              </div>
-            </li>
-            <li>
-              <div class="flex justify-end text-gray-400 text-sm">
-                <a class="mx-2" href="https://www.v3r.net/a-propos-de-la-ville/communications" class="hover:underline">Communications</a> |
-                <a class="mx-2" href="https://www.v3r.net/faq" class="hover:underline">FAQ</a> |
-                <a class="mx-2" href="https://www.tourismetroisrivieres.com/fr" class="hover:underline">Tourisme</a>
-              </div>
-            </li>
-            <li>
-              <div class="flex justify-end text-gray-400 text-sm">
-                <a class="mx-2" href="https://v3r.sharepoint.com/" class="hover:underline">Intranet</a> |
-                <a class="mx-2" href="https://organismesv3r.net/" class="hover:underline">Portail d'accès aux organismes</a> |
-                <a class="mx-2" href="https://www.v3r.net/politique-de-confidentialite" class="hover:underline">Politique de confidentialité</a>
-              </div>
-            </li>
-            <li>
-              <div class="flex justify-end text-gray-400 text-sm">© Ville de Trois-Rivières. Tous droit réservés.</div>
-            </li>
-          </ul>
+      <div class="flex flex-col justify-end w-2/5 mb-20 mt-8 mr-6 text-gray-400">
+        <ul class="text-base font-raleway">
+          <li>
+            <div class="flex flex-row flex justify-end text-small font-raleway text-gray-600 mb-4">
+              <a class="mx-2" href="https://www.v3r.net/a-propos-de-la-ville/communications/infolettre">
+                <span class="iconify size-10" data-icon="mdi:email-newsletter" data-inline="false"></span>
+              </a>
+              <a class="mx-2" href="https://www.facebook.com/villetroisrivieres" class="">
+                <span class="iconify size-10" data-icon="akar-icons:facebook-fill" data-inline="false"></span>
+              </a>
+              <a class="mx-2" href="https://www.instagram.com/villedetroisrivieres/" class="">
+                <span class="iconify size-10" data-icon="bi:instagram" data-inline="false"></span>
+              </a>
+              <a class="mx-2" href="https://www.linkedin.com/company/ville-de-trois-rivi-res" class="">
+                <span class="iconify size-10" data-icon="ri:linkedin-fill" data-inline="false"></span>
+              </a>
+              <a class="mx-2" href="https://www.youtube.com/channel/UC4UyW0CoFiJaFCFaOzoQQ5w" class="">
+                <span class="iconify size-10" data-icon="fa:youtube" data-inline="false"></span>
+              </a>
+            </div>
+          </li>
+          <li>
+            <div class="flex justify-end text-sm">
+              <a class="mx-2" href="https://v3r.sharepoint.com/" class="hover:underline">Intranet</a> |
+              <a class="mx-2" href="https://organismesv3r.net/" class="hover:underline">Portail d'accès aux organismes</a> |
+              <a class="mx-2" href="https://www.v3r.net/politique-de-confidentialite" class="hover:underline">Politique de confidentialité</a>
+            </div>
+          </li>
+          <li>
+            <div class="flex justify-end text-gray-400 text-sm">© Ville de Trois-Rivières. Tous droit réservés.</div>
+          </li>
+        </ul>
       </div>
     </div>
   </footer>
