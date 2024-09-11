45538acbe8ca10fe92362f5059558a1f2c09a0e4
Mousse <143189920+IamMousse@users.noreply.github.com>
2024-09-11 12:09:51 -0400

    wip footer
    -tentative de modification font-family mais bug biz

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,9 +22,9 @@
 
   @yield('contenu')
   @yield('footer')
-  <footer class="bg-gray-200 flex w-full raleway">
+  <footer class="bg-gray-200 flex w-full">
     <div class="w-full lg:p-8 lg:flex-row flex-col  flex  gap-8 p-4 ">
-      <div class="lg:flex justify-start hidden lg:pl-8  ">
+      <div class="lg:flex justify-start hidden lg:pl-8">
         <a href="https://www.v3r.net/">
           <img src="https://www.v3r.net/wp-content/themes/v3r/Images/icons/logo-v3r-v2017.svg" alt="V3R Logo" />
         </a>
@@ -50,13 +50,13 @@
         <div class="flex justify-center  lg:justify-start">
           <h4>Ville de Trois-Rivières</h4>
         </div>
-        <div class="flex text-md lg:text-sm justify-center  bg-white lg:bg-gray-200 text-center lg:text-start   p-2 lg:p-0 hover:bg-gray-100  lg:hover:bg-transparent">
+        <div class="flex text-md lg:text-sm justify-center  bg-white lg:bg-gray-200 text-center lg:text-start p-2 lg:p-0 hover:bg-gray-100 lg:hover:bg-transparent">
           <a href="https://www.google.ca/maps/place/H%C3%B4tel+de+ville+de+Trois-Rivi%C3%A8res/@46.3430042,-72.545511,17z/data=!4m14!1m7!3m6!1s0x41aa0c6a9ae1712b:0xc5f7bf52c7282858!2sH%C3%B4tel+de+ville+de+Trois-Rivi%C3%A8res!8m2!3d46.3435631!4d-72.5429808!16s%2Fg%2F11bwpv19ny!3m5!1s0x41aa0c6a9ae1712b:0xc5f7bf52c7282858!8m2!3d46.3435631!4d-72.5429808!16s%2Fg%2F11bwpv19ny?entry=ttu&g_ep=EgoyMDI0MDkwNC4wIKXMDSoASAFQAw%3D%3D"
             class="hover:underline  ">
             1325, place de l'Hôtel-de-Ville, C.P.368 <br class="lg:hidden"> Trois-Rivières, QC G9A 5H3
           </a>
         </div>
-        <div class="flex text-md lg:text-sm justify-center lg:justify-start  bg-white lg:bg-gray-200    p-2 lg:p-0 hover:bg-gray-100  lg:hover:bg-transparent">
+        <div class="flex text-md lg:text-sm justify-center lg:justify-start  bg-white lg:bg-gray-200 p-2 lg:p-0 hover:bg-gray-100  lg:hover:bg-transparent">
           <h4>
             <b>Téléphone:
               <a class="mx-1" href="tel:311" class="hover:underline">311</a>
@@ -65,10 +65,10 @@
             </b>
           </h4>
         </div>
-        <div class="flex text-md lg:text-sm justify-center lg:justify-start  bg-white lg:bg-gray-200  p-2 lg:p-0 hover:bg-gray-100  lg:hover:bg-transparent">
+        <div class="flex text-md lg:text-sm justify-center lg:justify-start bg-white lg:bg-gray-200  p-2 lg:p-0 hover:bg-gray-100  lg:hover:bg-transparent">
           <a href="tel:18333742002" class="hover:underline me-4 md:me-6">Canada ou États-Unis: 1833 374-2002</a>
         </div>
-        <div class="flex text-md lg:text-sm justify-center lg:justify-start  bg-white lg:bg-gray-200   p-2 lg:p-0 hover:bg-gray-100  lg:hover:bg-transparent">
+        <div class="flex text-md lg:text-sm justify-center lg:justify-start bg-white lg:bg-gray-200   p-2 lg:p-0 hover:bg-gray-100  lg:hover:bg-transparent">
           <a href="mailto:311@v3r.net" class="hover:underline me-4 md:me-6">Courriel: 311@v3r.net</a>
         </div>
       </div>
@@ -80,7 +80,7 @@
           <a href="https://www.tourismetroisrivieres.com/fr" class="hover:underline">Tourisme</a>
         </div>
       </div>
-      <div class="flex flex-col w-full  ">
+      <div class="flex flex-col w-full">
         <div class="flex flex-row  justify-end text-small font-raleway text-gray-600 mb-4">
           <div class="lg:flex hidden">
             <a class="mx-2" href="https://www.v3r.net/a-propos-de-la-ville/communications/infolettre">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Test')
 
 @section('header')
-<h1>HEADER</h1>
+<h1 class="font-raleway">HEADER</h1>
 
 @section('contenue')
 <div class="bg-lime-500">CONTENUE</div>
