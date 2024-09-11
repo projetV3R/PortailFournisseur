eca23b30aa33fa8b8ab62677b35f66637c0b3328
Mousse <143189920+IamMousse@users.noreply.github.com>
2024-09-11 16:43:17 -0400

    wip footer
    -responsive perf.
    -manque fontfamily mais tout est good

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,13 +22,13 @@
   @yield('contenu')
   @yield('footer')
   <footer class="bg-gray-200 flex w-full">
-    <div class="w-full lg:p-8 lg:flex-row flex-col  flex  gap-8 p-4 ">
-      <div class="lg:flex justify-start hidden lg:pl-8">
+    <div class="w-full md:p-8 md:flex-row flex-col flex gap-8 p-4">
+      <div class="md:flex justify-start hidden md:pl-8">
         <a href="https://www.v3r.net/">
-          <img src="https://www.v3r.net/wp-content/themes/v3r/Images/icons/logo-v3r-v2017.svg" alt="V3R Logo" />
+          <img class="md:h-20 md:w-20" src="https://www.v3r.net/wp-content/themes/v3r/Images/icons/logo-v3r-v2017.svg" alt="V3R Logo" />
         </a>
       </div>
-      <div class="lg:hidden flex gap-4 justify-center items-center">
+      <div class="md:hidden flex gap-4 justify-center items-center">
         <a href="https://www.v3r.net/a-propos-de-la-ville/communications/infolettre">
           <span class="iconify size-8" data-icon="mdi:email-newsletter" data-inline="false"></span>
         </a>
@@ -45,29 +45,29 @@
           <span class="iconify size-8" data-icon="fa:youtube" data-inline="false"></span>
         </a>
       </div>
-      <div class="flex flex-col justify-start lg:max-w-80 gap-2 lg:gap-0 pr-10 pb-2 pl-2">
-        <div class="flex justify-center  lg:justify-start">
+      <div class="flex flex-col justify-start md:max-w-80 gap-2 md:gap-0 pr-5 pb-2 pl-2">
+        <div class="flex justify-center  md:justify-start">
           <h4>Ville de Trois-Rivières</h4>
         </div>
-        <div class="flex text-md lg:text-sm justify-center  bg-white lg:bg-gray-200 text-center lg:text-start p-2 lg:p-0 hover:bg-gray-100 lg:hover:bg-transparent">
+        <div class="flex text-md sm:justify-center sm:text-center md:text-sm justify-center bg-white md:bg-gray-200 text-center md:text-start p-2 md:p-0 hover:bg-gray-100 md:hover:bg-transparent">
           <a href="https://www.google.ca/maps/place/H%C3%B4tel+de+ville+de+Trois-Rivi%C3%A8res/@46.3430042,-72.545511,17z/data=!4m14!1m7!3m6!1s0x41aa0c6a9ae1712b:0xc5f7bf52c7282858!2sH%C3%B4tel+de+ville+de+Trois-Rivi%C3%A8res!8m2!3d46.3435631!4d-72.5429808!16s%2Fg%2F11bwpv19ny!3m5!1s0x41aa0c6a9ae1712b:0xc5f7bf52c7282858!8m2!3d46.3435631!4d-72.5429808!16s%2Fg%2F11bwpv19ny?entry=ttu&g_ep=EgoyMDI0MDkwNC4wIKXMDSoASAFQAw%3D%3D"
             class="hover:underline">
             1325, place de l'Hôtel-de-Ville, C.P.368 <br class="lg:hidden"> Trois-Rivières, QC G9A 5H3
           </a>
         </div>
-        <div class="flex text-md lg:text-sm justify-center lg:justify-start  bg-white lg:bg-gray-200 p-2 lg:p-0 hover:bg-gray-100  lg:hover:bg-gray-100">
+        <div class="flex text-md md:text-sm justify-center md:justify-start bg-white md:bg-gray-200 p-2 md:p-0 hover:bg-gray-100 md:hover:bg-transparent">
           <h4>
             <b>Téléphone:
-              <a class="mx-1" href="tel:311" class="hover:underline">311</a>
+              <a href="tel:311" class="mx-1 hover:underline">311</a>
               ou
-              <a class="mx-1" href="tel:8193742002" class="hover:underline me-4 md:me-6">819 374-2002</a>
+              <a href="tel:8193742002" class="mx-1 hover:underline me-4 md:me-6">819 374-2002</a>
             </b>
           </h4>
         </div>
-        <div class="flex text-md lg:text-sm justify-center lg:justify-start bg-white lg:bg-gray-200  p-2 lg:p-0 hover:bg-gray-100  lg:hover:bg-transparent">
+        <div class="flex text-md md:text-sm justify-center md:justify-start bg-white md:bg-gray-200 p-2 md:p-0 hover:bg-gray-100 md:hover:bg-transparent">
           <a href="tel:18333742002" class="hover:underline ">Canada ou États-Unis: 1833 374-2002</a>
         </div>
-        <div class="flex text-md lg:text-sm justify-center lg:justify-start bg-white lg:bg-gray-200   p-2 lg:p-0 hover:bg-gray-100  lg:hover:bg-transparent">
+        <div class="flex text-md md:text-sm justify-center md:justify-start bg-white md:bg-gray-200 p-2 md:p-0 hover:bg-gray-100 md:hover:bg-transparent">
           <a href="mailto:311@v3r.net" class="hover:underline me-4 md:me-6">Courriel: 311@v3r.net</a>
         </div>
       </div>
@@ -80,34 +80,34 @@
         </div>
       </div>
 
-      <div class="flex flex-col w-full">
+      <div class="flex flex-col w-full md:w-80 lg:w-full ml-6">
         <div class="flex flex-row  justify-end text-small font-raleway text-gray-800 mb-4">
-          <div class="lg:flex hidden">
+          <div class="md:flex hidden">
             <a class="mx-2" href="https://www.v3r.net/a-propos-de-la-ville/communications/infolettre">
-              <span class="iconify size-10" data-icon="mdi:email-newsletter" data-inline="false"></span>
+              <span class="iconify size-10 md:size-8" data-icon="mdi:email-newsletter" data-inline="false"></span>
             </a>
             <a class="mx-2" href="https://www.facebook.com/villetroisrivieres" class="">
-              <span class="iconify size-10" data-icon="akar-icons:facebook-fill" data-inline="false"></span>
+              <span class="iconify size-10 md:size-8" data-icon="akar-icons:facebook-fill" data-inline="false"></span>
             </a>
             <a class="mx-2" href="https://www.instagram.com/villedetroisrivieres/" class="">
-              <span class="iconify size-10" data-icon="bi:instagram" data-inline="false"></span>
+              <span class="iconify size-10 md:size-8" data-icon="bi:instagram" data-inline="false"></span>
             </a>
             <a class="mx-2" href="https://www.linkedin.com/company/ville-de-trois-rivi-res" class="">
-              <span class="iconify size-10" data-icon="ri:linkedin-fill" data-inline="false"></span>
+              <span class="iconify size-10 md:size-8" data-icon="ri:linkedin-fill" data-inline="false"></span>
             </a>
             <a class="mx-2" href="https://www.youtube.com/channel/UC4UyW0CoFiJaFCFaOzoQQ5w" class="">
-              <span class="iconify size-10" data-icon="fa:youtube" data-inline="false"></span>
+              <span class="iconify size-10 md:size-8" data-icon="fa:youtube" data-inline="false"></span>
             </a>
           </div>
         </div>
 
-        <div class="xl:hidden lg:justify-end text-gray-400 text-sm flex justify-center ">
+        <div class="xl:hidden md:justify-end text-gray-400 text-sm flex justify-center">
           <a class="mx-2" href="https://www.v3r.net/a-propos-de-la-ville/communications" class="hover:underline">Communications</a> |
           <a class="mx-2" href="https://www.v3r.net/faq" class="hover:underline">FAQ</a> |
           <a class="mx-2" href="https://www.tourismetroisrivieres.com/fr" class="hover:underline">Tourisme</a>
         </div>
 
-        <div class="flex lg:justify-end text-gray-400 text-sm justify-center ">
+        <div class="flex justify-center md:justify-end text-gray-400 text-sm ">
           <div class="hover:underline">
             <a class="mx-2" href="https://v3r.sharepoint.com/">Intranet</a>|
           </div>
@@ -118,7 +118,7 @@
             <a class="mx-2" href="https://www.v3r.net/politique-de-confidentialite" class="hover:underline">Politique de confidentialité</a>
           </div>
         </div>
-        <div class="flex lg:justify-end justify-center text-gray-400 text-sm">© Ville de Trois-Rivières. Tous droit réservés.</div>
+        <div class="flex md:justify-end justify-center text-gray-400 text-sm">© Ville de Trois-Rivières. Tous droit réservés.</div>
 
 
       </div>
