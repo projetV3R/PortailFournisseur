fa7796903889aa5355adf59603214544179efd42
Mousse <143189920+IamMousse@users.noreply.github.com>
2024-10-02 09:26:07 -0400

    tentative de trie par javascript

--- app/Http/Controllers/CategorieUNSPSCcontroller.php
+++ app/Http/Controllers/CategorieUNSPSCcontroller.php
@@ -14,26 +14,37 @@ class CategorieUNSPSCcontroller extends Controller
     public function index()
     {
         $segments = Segment::All();
-        $families = Family::All();
-        $classes = Classe::All();
-        $commodities = Commodity::All();
+        //$families = Family::All(); $classes = Classe::All(); $commodities = Commodity::All();, 'families', 'classes', 'commodities'
 
-        return view('categorie', compact('segments', 'families', 'classes', 'commodities'));
+        return view('categorie', compact('segments'));
     }
 
-    /*
-    juste la bd unspsc
-
-    public function getProduits()
+    public function getFamilies(Request $request)
     {
-        $produits = CategorieUNSPSC::orderBy('segment')
-            ->orderBy('family')
-            ->orderBy('class')
-            ->get(['segment', 'segmentTitleFr', 'family', 'familyTitleFr', 'class', 'classTitleFr', 'commodity', 'commodityTitleFr']);
+        $families = Family::whereHas('segments', function ($query) use ($request) {
+            $query->where('segment', $request->segment);
+        })->get();
 
-        return response()->json($produits);
-    }*/
+        return response()->json($families);
+    }
 
+    public function getClasses(Request $request)
+    {
+        $classes = Classe::whereHas('families', function ($query) use ($request) {
+            $query->where('family', $request->family); 
+        })->get();
+
+        return response()->json($classes);
+    }
+
+    public function getCommodities(Request $request)
+    {
+        $commodities = Commodity::whereHas('classes', function ($query) use ($request) {
+            $query->where('class', $request->class);
+        })->get();
+
+        return response()->json($commodities);
+    }
 
     /**
      * Show the form for creating a new resource.
