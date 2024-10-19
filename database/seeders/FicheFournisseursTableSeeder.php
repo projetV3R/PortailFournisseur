6033d392f561f0727f5f622f2c04835ea9c3604e
Esdras Manefa <52791318+Manefa@users.noreply.github.com>
2024-10-19 16:23:47 -0400

    update des migrations

--- database/seeders/FicheFournisseursTableSeeder.php
+++ database/seeders/FicheFournisseursTableSeeder.php
@@ -18,7 +18,7 @@ class FicheFournisseursTableSeeder extends Seeder
                 'etat' => 'En attente',
                 'nom_entreprise' => 'Construction ABC Inc.',
                 'adresse_courriel' => 'contact@abcconstruction.com',
-                'mot_de_passe' => bcrypt('Abc123!@'),  // Respect des règles du mot de passe
+                'password' => bcrypt('Abc123!@'),  // Respect des règles du mot de passe
                 'details_specifications' => 'Construction de bâtiments commerciaux et résidentiels',
                 'date_changement_etat' => null,
                 'licence_id' => 1,
@@ -32,7 +32,7 @@ class FicheFournisseursTableSeeder extends Seeder
                 'etat' => 'En attente',
                 'nom_entreprise' => 'Rénovation Xyz Ltée',
                 'adresse_courriel' => 'info@renovxyz.ca',
-                'mot_de_passe' => bcrypt('Xyz456@!'),  // Respect des règles du mot de passe
+                'password' => bcrypt('Xyz456@!'),  // Respect des règles du mot de passe
                 'details_specifications' => 'Rénovation et agrandissement résidentiel',
                 'date_changement_etat' => null,
                 'licence_id' => 2,
@@ -46,7 +46,7 @@ class FicheFournisseursTableSeeder extends Seeder
                 'etat' => 'En attente',
                 'nom_entreprise' => 'Plomberie 123',
                 'adresse_courriel' => 'support@plomberie123.com',
-                'mot_de_passe' => bcrypt('Plomb789$#'),  // Respect des règles du mot de passe
+                'password' => bcrypt('Plomb789$#'),  // Respect des règles du mot de passe
                 'details_specifications' => 'Services de plomberie résidentielle et commerciale',
                 'date_changement_etat' => null,
                 'licence_id' => 3,
@@ -60,7 +60,7 @@ class FicheFournisseursTableSeeder extends Seeder
                 'etat' => 'En attente',
                 'nom_entreprise' => 'Électricité Pro',
                 'adresse_courriel' => 'contact@electropro.com',
-                'mot_de_passe' => bcrypt('ElecPro321@#'),  // Respect des règles du mot de passe
+                'password' => bcrypt('ElecPro321@#'),  // Respect des règles du mot de passe
                 'details_specifications' => 'Installation de systèmes électriques pour bâtiments industriels',
                 'date_changement_etat' => null,
                 'licence_id' => 4,
@@ -74,7 +74,7 @@ class FicheFournisseursTableSeeder extends Seeder
                 'etat' => 'En attente',
                 'nom_entreprise' => 'Excavation Max',
                 'adresse_courriel' => 'info@excavationmax.ca',
-                'mot_de_passe' => bcrypt('ExcaMax123$%'),  // Respect des règles du mot de passe
+                'password' => bcrypt('ExcaMax123$%'),  // Respect des règles du mot de passe
                 'details_specifications' => 'Excavation et terrassement',
                 'date_changement_etat' => null,
                 'licence_id' => 5,
--- database/seeders/TelephonesTableSeeder.php
+++ database/seeders/TelephonesTableSeeder.php
@@ -18,7 +18,6 @@ class TelephonesTableSeeder extends Seeder
                 'numero_telephone' => '5145551234',
                 'ligne' => 1,  // Valeur entière correspondant à la ligne
                 'poste' => '101',
-                'type_numero' => 'bureau', // Correspond à la colonne `type_numero`
                 'created_at' => '2024-09-18 10:00:00',
             ],
             [
@@ -26,7 +25,6 @@ class TelephonesTableSeeder extends Seeder
                 'numero_telephone' => '4185555678',
                 'ligne' => 2,
                 'poste' => '102',
-                'type_numero' => 'bureau',
                 'created_at' => '2024-09-18 10:30:00',
             ],
             [
@@ -34,7 +32,6 @@ class TelephonesTableSeeder extends Seeder
                 'numero_telephone' => '4505557890',
                 'ligne' => 3,
                 'poste' => '103',
-                'type_numero' => 'bureau',
                 'created_at' => '2024-09-18 11:00:00',
             ],
             [
@@ -42,7 +39,6 @@ class TelephonesTableSeeder extends Seeder
                 'numero_telephone' => '6135553456',
                 'ligne' => 4,
                 'poste' => '104',
-                'type_numero' => 'fixe',
                 'created_at' => '2024-09-18 11:30:00',
             ],
             [
@@ -50,7 +46,6 @@ class TelephonesTableSeeder extends Seeder
                 'numero_telephone' => '8195559876',
                 'ligne' => 5,
                 'poste' => '105',
-                'type_numero' => 'fixe',
                 'created_at' => '2024-09-18 12:00:00',
             ],
         ]);
