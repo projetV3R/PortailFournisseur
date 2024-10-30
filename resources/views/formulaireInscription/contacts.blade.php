4f2f077967d77aab07d8b57c6f3fb01298b9f607
Mousse <143189920+IamMousse@users.noreply.github.com>
2024-10-30 15:40:35 -0400

    update confirm button
    modification après review de la couleur du bouton confirmation

--- resources/views/formulaireInscription/contacts.blade.php
+++ resources/views/formulaireInscription/contacts.blade.php
@@ -204,7 +204,7 @@
                 text: "La suppression n'est pas réversible !",
                 icon: "warning",
                 showCancelButton: true,
-                confirmButtonColor: "#3085d6",
+                confirmButtonColor: "#0076D5",
                 cancelButtonColor: "#d33",
                 confirmButtonText: "Supprimer",
                 cancelButtonText: "Annuler"
