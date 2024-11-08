f8fd48c07b7d0859413460505748dcdc6079048a
Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
2024-11-07 21:54:45 -0500

    ajout gestion si user auth

--- app/Http/Controllers/FicheFournisseurController.php
+++ app/Http/Controllers/FicheFournisseurController.php
@@ -291,6 +291,9 @@ class FicheFournisseurController extends Controller
 
     public function redirection()
     {
+        if(Auth::check()){
+            return redirect()->route('profil');
+        }
 
         // Vérifie si la variable de session 'inscrit' est définie et à true
         if (!session('inscrit', false)) {
