b4a6a65a42bab8524025d3ab6ed7eeca681cb044
Mousse <143189920+IamMousse@users.noreply.github.com>
2024-11-27 15:53:28 -0500

    route fallback fini

---
+++ resources/views/redirection/404.blade.php
@@ -0,0 +1,19 @@
+
+<script src="https://code.iconify.design/3/3.0.0/iconify.min.js"></script>
+<script src="https://cdn.tailwindcss.com"></script>
+<link rel="icon" type="image/png" href="https://www.v3r.net/wp-content/uploads/2023/06/favicon.png" />
+<title>Erreur 404 - Page non trouvée</title>
+<div class="w-full">
+    <div class="absolute inset-x-0 z-10">
+        <a href="/" class="">
+            <span class="iconify size-10" data-icon="ion:arrow-undo" data-inline="false"></span>
+
+        </a>
+    </div>
+    <div class="relative h-screen flex flex-col items-center justify-center overflow-hidden ">
+        <img src="{{ asset('images/404.png') }}" alt="imageErreur403" class="relative z-10 h-auto max-w-full">
+        <span class="text-md md:text-2xl lg:text-4xl font-bold">Le motif de votre recherche n'a mené à aucun
+            résultat.</span>
+    </div>
+
+</div>
